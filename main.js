1d0e2b21050d9923e6bf38e120034dbc09dc8070
maya17power <9sanchez@protonmail.com>
2024-07-23 21:18:20 -0700

    updated maya branch to display tally up orders demo.

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 const API_URL = "https://menus-api.vercel.app/";
 const itemList = document.querySelector("#addItem");
+const addItemBtn = document.querySelector('#addItemBtn');
 
 async function getData() {
   try {
@@ -7,53 +8,53 @@ async function getData() {
     if (response.ok) {
       const data = await response.json();
       return data;
-    };
+    }
   } catch (error) {
     console.log(error);
-  };
-};
+  }
+}
 
 const menuData = getData();
 
-menuData.then((data)=>{
-    //iterrate through obj
-    //output ID and NAME
-    //send to be rendered
-    data.bbqs.map((i)=> {
-        console.log(i)
-        const {id, name, img, dsc, price} = i;
-        renderElement(id,name,img, dsc, price);
-    });
-    data.drinks.map((i)=> {
-      console.log(i)
-      const {id, name, img, dsc, price} = i;
-      renderElement(id,name,img, dsc, price);
+menuData.then((data) => {
+  //iterate through obj
+  //output ID and NAME
+  //send to be rendered
+  data.bbqs.map((i) => {
+    const { id, name, img, dsc, price } = i;
+    renderElement(id, name, img, dsc, price);
   });
-  data.desserts.map((i)=> {
-    console.log(i)
-    const {id, name, img, dsc, price} = i;
-    renderElement(id,name,img, dsc, price);
+  data.drinks.map((i) => {
+    const { id, name, img, dsc, price } = i;
+    renderElement(id, name, img, dsc, price);
+  });
+  data.desserts.map((i) => {
+    const { id, name, img, dsc, price } = i;
+    renderElement(id, name, img, dsc, price);
+  });
 });
 
-
-  
-});
-
-function renderElement(id, name, img, dsc, price){
-    //create new list element
-    //add card property to list item
-    //add id and name to element
-    //render list item card to page.
-    const listItem = document.createElement('li');
-    listItem.className = 'list-group-item';
-    listItem.innerHTML = `
+function renderElement(id, name, img, dsc, price) {
+  //create new list element
+  //add card property to list item
+  //add id and name to element
+  //render list item card to page.
+  const listItem = document.createElement("li");
+  listItem.className = "list-group-item";
+  listItem.innerHTML = `
     <li class='list-group-item'>ID: ${id}</li>
     <li class='list-group-item'>NAME: ${name}</li>
     <li class='list-group-item'>DESCRIPTION: ${dsc}</li>
     <img src="${img}" alt="${name}" class='list-group-img'/> 
     <li class='list-group-item'>PRICE: ${price}</li>
-
-
     `;
-    itemList.appendChild(listItem);
-};
+  itemList.appendChild(listItem);
+}
+
+addItemBtn.addEventListener('click',()=>{
+  let randomNum = Math.random() * 35;
+  let price = randomNum.toFixed(2);
+  console.log('$' + price);
+
+  
+});
