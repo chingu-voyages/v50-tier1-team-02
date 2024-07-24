869aaa492fa1374124b3779b3fbeabcdc96a695e
maya17power <9sanchez@protonmail.com>
2024-07-23 23:44:10 -0700

    updated tallyporder demo, add item and random price include total order price.

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 const API_URL = "https://menus-api.vercel.app/";
 const itemList = document.querySelector("#addItem");
 const addItemBtn = document.querySelector("#addItemBtn");
-const tallyUpOrder_list = document.querySelector("#tallyUpOrderList");
+const tallyUpOrders_list = document.querySelector("#tallyUpOrders_list");
 
 async function getData() {
   try {
@@ -52,20 +52,40 @@ function renderElement(id, name, img, dsc, price) {
   itemList.appendChild(listItem);
 }
 
+//total catcher
+let orderTotal = 0;
+
+//click button to add item and append price
 addItemBtn.addEventListener("click", () => {
-  const item = document.createElement("li");
-  item.className = "list-group-item bg-info";
-  item.innerHTML = `
-    <div class="row">
-    <div class="col">Item Name</div>
-    <div class="col text-end">${randomPrice()}</div>
-  </div>
-  `;
-  document.querySelector("#tallyUpOrders_list").appendChild(item);
+  renderItems("new Item", randomPrice());
 });
 
+//generate random price for item
 const randomPrice = () => {
   let randomNum = Math.random() * 35;
   let price = randomNum.toFixed(2);
-  return "$" + price;
+  return price * 1;
+};
+
+//render items from array
+function renderItems(item,price) {
+  const list_item = document.createElement("li");
+  list_item.className = "list-group-item bg-info";
+  list_item.innerHTML = `
+  <div class="row">
+  <div class="col">${item}</div>
+  <div class="col text-end itemPrice">$${price}</div>
+</div>
+`;
+  tallyUpOrders_list.appendChild(list_item);
+  
+  orderTotal += price;
+
+  tallyUpOrder(orderTotal.toFixed(2));
+
+};
+
+function tallyUpOrder (total){
+  const orderTotal = document.querySelector('#orderTotal');
+  orderTotal.innerHTML = `$${total}`
 };
